a3f1c37c1428ef0e74408f881f9bcba8ccd38e25
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-03-10 10:16:55 +0530

    Added

--- MedicalShopManagementSystem/MedicalShopManagementSystem/settings.py
+++ MedicalShopManagementSystem/MedicalShopManagementSystem/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['medishopmanagementsystem-production.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = [ 'https://web-production-bd820.up.railway.app' ]
+
 
 # Application definition
 
